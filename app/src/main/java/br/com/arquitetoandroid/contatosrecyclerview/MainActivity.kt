3ac32822ad6bf8a071949da2ee5e731a2636d097
Alimeidqueiroz <almeidaq.line@gmail.com>
2021-08-27 12:33:24 -0300

    ajuste activity_main

--- app/src/main/java/br/com/arquitetoandroid/contatosrecyclerview/MainActivity.kt
+++ app/src/main/java/br/com/arquitetoandroid/contatosrecyclerview/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity(), ClickItemContactListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.drawer_menu)
+        setContentView(R.layout.activity_main)
 
         initDrawer()
         fetchListcontact()
